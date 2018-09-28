608ebfafcb237bd2cac6924cf0396a3f6ebb367d
spoonnman <spoonnman@hotmail.com>
2018-09-28 01:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return kittens.splice[kittens.length, 0, name]
+  return kittens.splice[kittens.length - 1, 0, name]
 }
 
