65f8685e8cae8da8822b4801818bc16e80fd395f
spoonnman <spoonnman@hotmail.com>
2018-09-28 00:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ var kittens = []
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKittens(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
